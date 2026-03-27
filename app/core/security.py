dbabe608da90ad8cee8ed14ff5e8a2c2ee39441d
Suraj Kumar Jena <suraj.jena@cognecto.com>
2026-03-27 17:03:21 +0530

    modified password hashing

--- app/core/security.py
+++ app/core/security.py
@@ -7,10 +7,13 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 
 def hash_password(password: str) -> str:
+    # bcrypt max 72 bytes
+    password = password[:72]
     return pwd_context.hash(password)
 
 
 def verify_password(plain_password: str, hashed_password: str) -> bool:
+    plain_password = plain_password[:72]
     return pwd_context.verify(plain_password, hashed_password)
 
 
