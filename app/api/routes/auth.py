832bdd15c8a741973c5a6b1fc6522fa8c4198db0
Suraj Kumar Jena <suraj.jena@cognecto.com>
2026-03-27 18:39:01 +0530

    added getUser api

--- app/api/routes/auth.py
+++ app/api/routes/auth.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
-from app.api.deps import get_db
+from app.api.deps import get_db, get_current_user
 from app.schemas.auth import RegisterRequest, LoginRequest, TokenResponse
 from app.services.auth_service import register_user, login_user
 
@@ -19,3 +19,12 @@ def register(data: RegisterRequest, db: Session = Depends(get_db)):
 @router.post("/login", response_model=TokenResponse)
 def login(data: LoginRequest, db: Session = Depends(get_db)):
     return login_user(db, data.username, data.password)
+
+@router.get("/getUser")
+def get_me(current_user = Depends(get_current_user)):
+    return {
+        "id": current_user.id,
+        "username": current_user.username,
+        "email": current_user.email,
+        "mobile": current_user.mobile
+    }
