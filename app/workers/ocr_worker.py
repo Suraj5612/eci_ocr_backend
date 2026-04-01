9b2660ce3b9f213f17bc0e7d6499d13862cb5cbe
Suraj Kumar Jena <suraj.jena@cognecto.com>
2026-04-01 23:51:36 +0530

    regex fix

--- app/workers/ocr_worker.py
+++ app/workers/ocr_worker.py
@@ -45,10 +45,10 @@ def process_job(job: Job, db: Session):
             top_left, form_section = crop_rois(image)
 
             # printed
-            top_left = enhance_cropped(top_left)
+            #top_left = enhance_cropped(top_left)
 
             # handwritten (you can later improve)
-            form_section = enhance_handwritten(form_section)
+            #form_section = enhance_handwritten(form_section)
 
             # ensure same width
             w = max(top_left.shape[1], form_section.shape[1])
@@ -57,14 +57,15 @@ def process_job(job: Job, db: Session):
             form_section_resized = cv2.resize(form_section, (w, form_section.shape[0]))
 
             # ensure same type (grayscale)
-            if len(top_left_resized.shape) == 2:
-                top_left_resized = cv2.cvtColor(top_left_resized, cv2.COLOR_GRAY2BGR)
+            #if len(top_left_resized.shape) == 2:
+                #top_left_resized = cv2.cvtColor(top_left_resized, cv2.COLOR_GRAY2BGR)
 
-            if len(form_section_resized.shape) == 2:
-                form_section_resized = cv2.cvtColor(form_section_resized, cv2.COLOR_GRAY2BGR)
+            #if len(form_section_resized.shape) == 2:
+                #form_section_resized = cv2.cvtColor(form_section_resized, cv2.COLOR_GRAY2BGR)
 
             # concat
             processed = cv2.vconcat([top_left_resized, form_section_resized])
+            #processed = image
 
         # 🔥 4. CALL SARVAM OCR
         print("🧠 Calling Sarvam OCR...")
